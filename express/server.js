ab6d6317b01fd1d0160fb6c61aad6675366b6bf4
Shubh Palan <63784377+shubh-palan@users.noreply.github.com>
2021-10-18 03:45:02 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -18,5 +18,26 @@ app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
+
+const port = 9000;
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/get-token", (req, res) => {
+  const API_KEY = "9dc7ab5b-c227-4f0f-a0fe-8ff279cdaf75";
+  const SECRET_KEY = "2e3812d57719479b3a35110d7fb9d51921c16f3ce25bd5374504b96dc9ce528a";
+  const options = { expiresIn: "10d", algorithm: "HS256" };
+  const payload = {
+    apikey: API_KEY,
+    permissions: ["allow_join", "allow_mod", "ask_join"], // Trigger permission.
+  };
+  const token = jwt.sign(payload, SECRET_KEY, options);
+  res.json({ token });
+});
+
+
+
+
 module.exports = app;
 module.exports.handler = serverless(app);
